dfa8e29ead6e5040cfce0920da81ddf7903dd6dd
unknown <corymaklin@gmail.com>
2021-11-14 14:51:12 -0500

    2 level system

---
+++ Assets/LevelSystem/Tests/Runtime/LevelControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace LevelSystem.Tests
+{
+    public class LevelControllerTests
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            EditorSceneManager.LoadSceneInPlayMode("Assets/LevelSystem/Tests/Scenes/Test.unity",
+                new LoadSceneParameters(LoadSceneMode.Single));
+        }
+
+        [UnityTest]
+        public IEnumerator LevelController_WhenLevelUp_UpdateRequiredExperience()
+        {
+            yield return null;
+            LevelController levelController = GameObject.FindObjectOfType<LevelController>();
+            Assert.AreEqual(1, levelController.level);
+            levelController.currentExperience += 83;
+            Assert.AreEqual(2, levelController.level);
+            Assert.AreEqual(0, levelController.currentExperience);
+            Assert.AreEqual(92, levelController.requiredExperience);
+        }
+    }
+}
