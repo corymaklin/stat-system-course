e313b3d040baebe13766e84adab2f7e919b3d186
unknown <corymaklin@gmail.com>
2021-11-20 11:25:21 -0500

    1 gameplay

---
+++ Assets/MyGame/Scripts/Runtime/AnimationController.cs
@@ -0,0 +1,60 @@
+﻿using StatSystem;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MyGame
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(StatController))]
+    public class AnimationController : MonoBehaviour
+    {
+        [SerializeField] private float m_BaseSpeed = 3.5f;
+        private Animator m_Animator;
+        private NavMeshAgent m_NavMeshAgent;
+        private StatController m_StatController;
+        private static readonly int MOVEMENT_SPEED = Animator.StringToHash("MovementSpeed");
+        private static readonly int VELOCITY = Animator.StringToHash("Velocity");
+
+        private void Awake()
+        {
+            m_Animator = GetComponent<Animator>();
+            m_NavMeshAgent = GetComponent<NavMeshAgent>();
+            m_StatController = GetComponent<StatController>();
+        }
+
+        private void Update()
+        {
+            m_Animator.SetFloat(VELOCITY, m_NavMeshAgent.velocity.magnitude / m_NavMeshAgent.speed);
+        }
+
+        private void OnEnable()
+        {
+            m_StatController.initialized += OnStatControllerInitialized;
+            if (m_StatController.isInitialized)
+            {
+                OnStatControllerInitialized();
+            }
+        }
+
+        private void OnDisable()
+        {
+            m_StatController.initialized -= OnStatControllerInitialized;
+            if (m_StatController.isInitialized)
+            {
+                m_StatController.stats["MovementSpeed"].valueChanged -= OnMovementSpeedChanged;
+            }
+        }
+
+        private void OnStatControllerInitialized()
+        {
+            OnMovementSpeedChanged();
+            m_StatController.stats["MovementSpeed"].valueChanged += OnMovementSpeedChanged;
+        }
+
+        private void OnMovementSpeedChanged()
+        {
+            m_Animator.SetFloat(MOVEMENT_SPEED, m_StatController.stats["MovementSpeed"].value / 100f);
+            m_NavMeshAgent.speed = m_BaseSpeed * m_StatController.stats["MovementSpeed"].value / 100f;
+        }
+    }
+}
--- Assets/MyGame/Scripts/Runtime/Player.cs
+++ Assets/MyGame/Scripts/Runtime/Player.cs
@@ -1,19 +1,30 @@
 ﻿using LevelSystem;
 using StatSystem;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace MyGame.Scripts
 {
     [RequireComponent(typeof(PlayerStatController))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class Player : MonoBehaviour
     {
         private PlayerStatController m_PlayerStatController;
         private ILevelable m_Levelable;
+        [SerializeField] private Transform m_Target;
+        private NavMeshAgent m_NavMeshAgent;
 
         private void Awake()
         {
             m_PlayerStatController = GetComponent<PlayerStatController>();
             m_Levelable = m_PlayerStatController.GetComponent<ILevelable>();
+            m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            if (m_Target != null)
+                m_NavMeshAgent.SetDestination(m_Target.position);
         }
 
         private void Update()
